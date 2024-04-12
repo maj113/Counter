8059fbc0b02fc5cf5e4a0a1bcba142b6ae5997f5
Maj <majs.1236@gmail.com>
2024-04-12 19:50:44 +0200

    Bugfix: overcouting when multithreaded is fixed Linting: minor codestyle changes and redundancies removed

--- counter.cpp
+++ counter.cpp
@@ -7,18 +7,17 @@
 
 // @powturbo's code with slight modifications  
 inline uint64_t opt_count(const char *s, const char *e, const char c) {
-    __m256i cv = _mm256_set1_epi8(c), zv = _mm256_setzero_si256(), sum = zv, acr0, acr1, acr2, acr3;
+    const __m256i cv = _mm256_set1_epi8(c), zv = _mm256_setzero_si256();
+    __m256i sum = zv, acr0, acr1, acr2, acr3;
 
-    const char *p, *pe;
-    for (p = s; p != e - ((e - s) % (252 * 32));) {
-        for (acr0 = acr1 = acr2 = acr3 = zv, pe = p + 252 * 32; p != pe; p += 128) {
-            // lddqu and stream_load would be faster on MSVC
-            acr0 = _mm256_sub_epi8(acr0, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) p)));
-            acr1 = _mm256_sub_epi8(acr1, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) (p + 32))));
-            acr2 = _mm256_sub_epi8(acr2, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) (p + 64))));
-            acr3 = _mm256_sub_epi8(acr3, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) (p + 96))));
-            // MSVC might not compile the code with cast to void
-            _mm_prefetch((const void *) (p + 1024), _MM_HINT_T0);
+    const char *pe;
+    while (s != e - (e - s) % (252 * 32)) {
+        for (acr0 = acr1 = acr2 = acr3 = zv, pe = s + 252 * 32; s != pe; s += 128) {
+            acr0 = _mm256_sub_epi8(acr0, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) s)));
+            acr1 = _mm256_sub_epi8(acr1, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) (s + 32))));
+            acr2 = _mm256_sub_epi8(acr2, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) (s + 64))));
+            acr3 = _mm256_sub_epi8(acr3, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) (s + 96))));
+            _mm_prefetch(s + 1024, _MM_HINT_T0);
         }
         sum = _mm256_add_epi64(sum, _mm256_sad_epu8(acr0, zv));
         sum = _mm256_add_epi64(sum, _mm256_sad_epu8(acr1, zv));
@@ -26,15 +25,18 @@ inline uint64_t opt_count(const char *s, const char *e, const char c) {
         sum = _mm256_add_epi64(sum, _mm256_sad_epu8(acr3, zv));
     }
 
-    for (acr0 = zv; p < e; p += 32)
-        acr0 = _mm256_sub_epi8(acr0, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) p)));
+    for (acr0 = zv; s + 32 < e; s += 32)
+        acr0 = _mm256_sub_epi8(acr0, _mm256_cmpeq_epi8(cv, _mm256_load_si256((const __m256i *) s)));
     sum = _mm256_add_epi64(sum, _mm256_sad_epu8(acr0, zv));
 
-    size_t count =
-            _mm256_extract_epi64(sum, 0)
-            + _mm256_extract_epi64(sum, 1)
-            + _mm256_extract_epi64(sum, 2)
-            + _mm256_extract_epi64(sum, 3);
+    uint64_t count =
+        _mm256_extract_epi64(sum, 0)
+        + _mm256_extract_epi64(sum, 1)
+        + _mm256_extract_epi64(sum, 2)
+        + _mm256_extract_epi64(sum, 3);
+
+    while(s != e) 
+        count += *s++ == c;
 
     return count;
 }
@@ -43,9 +45,9 @@ uint64_t opt_count_parallel(const char *begin, const char *end, const char targe
     const unsigned int num_threads = std::thread::hardware_concurrency();
     const size_t total_length = end - begin;
 
-    if ((total_length < num_threads * 2) or singleThreaded)
+    if (total_length < num_threads * 2 or singleThreaded)
         return opt_count(begin, end, target);
-    // TODO: better/more precise chunk size calculation needed
+    
     const size_t chunk_size = (total_length + num_threads - 1) / num_threads;
 
     std::vector<std::thread> threads;
@@ -54,10 +56,10 @@ uint64_t opt_count_parallel(const char *begin, const char *end, const char targe
     uint64_t total_count = 0;
 
     for (size_t i = 0; i < num_threads; ++i) {
-        const char *chunk_begin = begin + (i * chunk_size);
+        const char *chunk_begin = begin + i * chunk_size;
         const char *chunk_end = std::min(end, chunk_begin + chunk_size);
 
-        threads.emplace_back([&total_count, chunk_begin, chunk_end, target]() {
+        threads.emplace_back([&total_count, chunk_begin, chunk_end, target] {
             total_count += opt_count(chunk_begin, chunk_end, target);
         });
     }
